1e39504c92868c91c6b6c42ca4f0abefe8d2d472
daviddeng@lianwutech.com <david.bb@2013>
2014-07-31 16:39:24 +0800

    功能初步实现

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 plugin_serial_dtu
 ==================
 串口访问modbus网络
+
--- serial_dtu.py
+++ serial_dtu.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-    感知无限公司
-    1、device_id的组成方式为ip_port
-    2、设备类型为0，未知
-    3、通过全局变量来管理每个介入设备，信息包括device_id，thread对象，handler对象
-    4、设备数据传输格式：
-        对于内置传感器：字符串不变；整形转成字符串；二进制转成16进制字符串
-        对于透传数据：data为数据的16进制字符串
-    5、设备指令传输格式：
-        对于继电器控制，device_cmd内容为json字符串，ctrl_cmd："0x1a"／"0x2a"／"0x3b",ctrl_param:整形字符串如"10"
-        对于透传指令，device_cmd为透传的指令
-    6、devices_info_dict需要持久化设备信息，启动时加载，变化时写入
+    modbus网络的串口数据采集插件
+    1、device_id的组成方式为ip_port_slaveid
+    2、设备类型为0，协议类型为modbus
+    3、devices_info_dict需要持久化设备信息，启动时加载，变化时写入
+    4、device_cmd内容：json字符串
 """
-import sys
-import os
 import time
+import json
 import serial
 import paho.mqtt.client as mqtt
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
@@ -38,10 +31,7 @@ except ImportError:
 from json import loads, dumps
 
 from libs.utils import *
-from libs.znetmsgdefine import *
-from libs.znetmessage import *
-from libs.serialcommunicate import *
-from libs.platformdevicedefine import *
+from libs.modbusdefine import *
 
 # 全局变量
 # 设备信息字典
@@ -69,6 +59,7 @@ serial_baund = int(config.get('serial', 'baund'))
 mqtt_server_ip = config.get('mqtt', 'server')
 mqtt_server_port = int(config.get('mqtt', 'port'))
 gateway_topic = config.get('gateway', 'topic')
+data_protocol = config.get('protocol', 'data')
 
 # 获取本机ip
 ip_addr = get_ip_addr()
@@ -84,6 +75,9 @@ def load_devices_info_dict():
         devices_file.close()
         try:
             devices_info_dict = loads(content)
+            # 将每个设备设置成未激活状态
+            for device_id in devices_info_dict:
+                devices_info_dict[device_id]["active"] = False
         except Exception, e:
             logger.error("devices.txt内容格式不正确")
 
@@ -97,12 +91,15 @@ def check_device(device_id, device_type, device_addr, device_port):
             "device_id": device_id,
             "device_type": device_type,
             "device_addr": device_addr,
-            "device_port": device_port
+            "device_port": device_port,
+            "active": True
         }
         #写文件
         devices_file = open(devices_info_file, "w+")
         devices_file.write(dumps(devices_info_dict))
         devices_file.close()
+    else:
+        devices_info_dict[device_id]["active"] = True
 
 
 def publish_device_data(device_id, device_type, device_addr, device_port, device_data):
@@ -116,25 +113,6 @@ def publish_device_data(device_id, device_type, device_addr, device_port, device
                    hostname=mqtt_server_ip,
                    port=mqtt_server_port)
     logger.info("向Topic(%s)发布消息：%s" % (gateway_topic, device_msg))
-
-
-def process_dtu_data(serial_data):
-    """
-    串口数据处理
-    :param data_sender:
-    :param serial_data:
-    :return:
-    """
-    # modbus解码
-
-    # 组消息包
-    device_id = "%s_%s_%s" % (ip_addr, serial_port, "0")
-    device_addr = serial_port
-    device_port = serial_baund
-    device_type = 0
-    device_data = serial_data
-    check_device(device_id, device_type, device_addr, device_port)
-    publish_device_data(device_id, device_type, device_addr, device_port, device_data)
 
 
 # 串口数据读取线程
@@ -151,59 +129,122 @@ def process_mqtt(device_id):
         for device_id in devices_info_dict:
             device_info = devices_info_dict[device_id]
             logger.debug("device_info:%r" % device_info)
-            if device_info["device_type"] == const.DEVICE_TYPE_DELAY_CTRL or \
-                            device_info["device_type"] == const.ZNET_DEVICE_TYPE_SENSOR_NONE:
-                mqtt_client.subscribe(device_id)
+            mqtt_client.subscribe(device_id)
 
     # The callback for when a PUBLISH message is received from the server.
     def on_message(client, userdata, msg):
         logger.info("收到数据消息" + msg.topic + " " + str(msg.payload))
         # 消息只包含device_cmd，为json字符串
-        device_cmd = loads(msg.payload)
-
-        # 根据topic确定设备由当前进程负责
-        # 透传指令
-
-
-
-        zigbee_ctrl_msg = GZXXZigbeeNetMessage()
-
-        device_info = devices_info_dict["device_id"]
-        node_port = device_info["device_port"]
-        # 指令消息处理对象
-        if len(device_info["device_addr"]) == 0:
-            zigbee_ctrl_msg.msg['tag_data']['tag_addr'] = "FFFFFF"
-        else:
-            # 根据协议，读卡器地址前两个字节为网络地址，第三个字节统一为0xef
-            zigbee_ctrl_msg.msg['tag_data']['tag_addr'] = device_info["device_addr"]
-
-        zigbee_ctrl_msg.msg['frame_type'] = zigbee_ctrl_msg.msg_engine.msg_def.const.FH_READER
-        zigbee_ctrl_msg.msg['tag_data_type'] = zigbee_ctrl_msg.msg_engine.msg_def.const.MESSAGE_TYPE_RELAY_CTRL
-        # 0x2切换到手动状态
-        zigbee_ctrl_msg.msg['tag_data']['control_mode_switch'] = 2
-
-        _delay_ctrl_reg = []
-        for i in range(0, 8):
-            ctrl_reg = dict()
-            if i == node_port - 1:
-                ctrl_reg["ctrl_cmd"] = int(device_cmd["ctrl_cmd"], 16)
-                ctrl_reg["ctrl_delay_time"] = int(device_cmd["ctrl_delay_time"])
-            else:
-                ctrl_reg["ctrl_cmd"] = 0xff
-                ctrl_reg["ctrl_delay_time"] = 0xffff
-            logger.debug("%d delay_ctrl_reg: %s" % (i, ctrl_reg))
-            _delay_ctrl_reg.append(ctrl_reg)
-        zigbee_ctrl_msg.msg['tag_data']['delay_ctrl_reg'] = _delay_ctrl_reg
-
-        # 消息打包
         try:
-            # logger.debug("zigbee_msg: %s" % dumps(zigbee_ctrl_msg.msg['tag_data']))
-            zigbee_ctrl_msg.pack()
-            logger.debug("打包后结果:%r" % zigbee_ctrl_msg.msg['frame_data_buff'])
-            # 指令下发
-            cooperator_serial.send(zigbee_ctrl_msg.msg['frame_data_buff'])
+            device_cmd = loads(msg.payload)
         except Exception, e:
-            logger.error("消息打包错误，错误内容：%s" % e)
+            device_cmd = None
+            logger.error("消息内容错误，%r" % msg.payload)
+
+        # 根据topic确定设备
+        device_info = devices_info_dict.get(msg.topic, None)
+        # 对指令进行处理
+        if device_info is not None:
+            if device_cmd["function_code"] == const.fc_read_coils or \
+                            device_cmd["function_code"] == const.fc_read_discrete_inputs:
+                req_result = modbus_client.read_coils(device_cmd["address"],
+                                                      device_cmd["count"],
+                                                      unit=device_info["device_port"])
+                device_data = {
+                    "func_code": device_cmd["function_code"],
+                    "addr": device_cmd["address"],
+                    "count": device_cmd["count"],
+                    "values": req_result.bits
+                }
+
+            elif device_cmd["function_code"] == const.fc_write_coil:
+                req_result = modbus_client.write_coil(device_cmd["address"],
+                                                      device_cmd["value"],
+                                                      unit=device_info["device_port"])
+                res_result = modbus_client.read_coils(device_cmd["address"],
+                                                      1,
+                                                      unit=device_info["device_port"])
+                device_data = {
+                    "func_code": device_cmd["function_code"],
+                    "addr": device_cmd["address"],
+                    "count": 1,
+                    "values": req_result.bits[0:1]
+                }
+            elif device_cmd["function_code"] == const.fc_write_coils:
+                req_result = modbus_client.write_coils(device_cmd["address"],
+                                                   device_cmd["values"],
+                                                   unit=device_info["device_port"])
+                counter = len(device_cmd["values"])
+                res_result = modbus_client.read_coils(device_cmd["address"],
+                                                      counter,
+                                                      unit=device_info["device_port"])
+                device_data = {
+                    "func_code": device_cmd["function_code"],
+                    "addr": device_cmd["address"],
+                    "count": counter,
+                    "values": res_result.bits
+                }
+            elif device_cmd["function_code"] == const.fc_write_register:
+                req_result = modbus_client.write_register(device_cmd["address"],
+                                                      device_cmd["value"],
+                                                      unit=device_info["device_port"])
+                res_result = modbus_client.read_holding_registers(device_cmd["address"],
+                                                      1,
+                                                      unit=device_info["device_port"])
+                device_data = {
+                    "func_code": device_cmd["function_code"],
+                    "addr": device_cmd["address"],
+                    "count": 1,
+                    "values": res_result.registers[0:1]
+                }
+            elif device_cmd["function_code"] == const.fc_write_registers:
+                result = modbus_client.write_registers(device_cmd["address"],
+                                                       device_cmd["values"],
+                                                       unit=device_info["device_port"])
+                counter = len(device_cmd["values"])
+                res_result = modbus_client.read_input_registers(device_cmd["address"],
+                                                      counter,
+                                                      unit=device_info["device_port"])
+                device_data = {
+                    "func_code": device_cmd["function_code"],
+                    "addr": device_cmd["address"],
+                    "count": counter,
+                    "values": res_result.registers
+                }
+            elif device_cmd["function_code"] == const.fc_read_holding_registers:
+                res_result = modbus_client.read_holding_registers(device_cmd["address"],
+                                                                  device_cmd["count"],
+                                                                  unit=device_info["device_port"])
+                device_data = {
+                    "func_code": device_cmd["function_code"],
+                    "addr": device_cmd["address"],
+                    "count": device_cmd["count"],
+                    "values": res_result.registers
+                }
+            elif device_cmd["function_code"] == const.fc_read_input_registers:
+                res_result = modbus_client.read_input_registers(device_cmd["address"],
+                                                                device_cmd["count"],
+                                                                unit=device_info["device_port"])
+                device_data = {
+                    "func_code": device_cmd["function_code"],
+                    "addr": device_cmd["address"],
+                    "count": device_cmd["count"],
+                    "values": res_result.registers
+                }
+            else:
+                logger.error("不支持的modbus指令：%d" % device_cmd["function_code"])
+                device_data = None
+
+            if device_data is not None:
+                publish_device_data(device_info["device_id"],
+                                    device_info["device_type"],
+                                    device_info["device_addr"],
+                                    device_info["device_port"],
+                                    data_protocol,
+                                    json.dumps(device_data))
+        else:
+            logger.error("设备不存在，消息主题:%s" % msg.topic)
+
 
     mqtt_client = mqtt.Client(client_id=gateway_topic)
     mqtt_client.on_connect = on_connect
@@ -220,9 +261,6 @@ def process_mqtt(device_id):
 
 if __name__ == "__main__":
 
-    # 加载设备数据
-    load_devices_info_dict()
-
     # 初始化modbus客户端
     modbus_client = ModbusClient(method='rtu', port=serial_port, baudrate=serial_baund, stopbits=serial.STOPBITS_ONE, parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS, timeout=1)
     modbus_client.connect()
@@ -231,40 +269,27 @@ if __name__ == "__main__":
     mqtt_thread = threading.Thread(target=process_mqtt)
     mqtt_thread.start()
 
+    # 加载设备数据
+    load_devices_info_dict()
 
-    data_buff = ""
+    # 更新设备状态
+    for slave_id in range(0, 0xff):
+        req_result = modbus_client.write_register(0, slave_id, unit=slave_id)
+        if req_result.function_code < 0x80:
+            device_id = "%s_%s_%d" % (ip_addr, serial_port, slave_id)
+            device_type = 0
+            device_addr = serial_port
+            device_port = slave_id
+            check_device(device_id, device_type, device_addr, device_port)
+            # 通知网关新增设备
+            publish_device_data(device_id, device_type, device_addr, device_port, "")
+
     while True:
-
         # 如果线程停止则创建
         if not mqtt_thread.is_alive():
             mqtt_thread = threading.Thread(target=process_mqtt)
             mqtt_thread.start()
 
-        # 读取串口数据并解析
-        # 串口数据读取
-        logger.debug("串口数据读取")
-        try:
-            serial_data = cooperator_serial.recv()
-        except Exception, e:
-            serial_data = ""
-            logger.error(e)
-            logger.info("重新打开串口")
-            result, error_info = cooperator_serial.open(serial_settings)
-            if result:
-                logger.error("重新打开串口成功")
-            else:
-                logger.error("重新打开串口失败")
-
-        # 解析串口数据
-        if len(serial_data) > 0:
-            logger.debug("串口数据: %r" % serial_data)
-            logger.debug("16进制串口数据: %s" % serial_data.encode("hex"))
-            logger.debug("串口数据长度: %d" % (len(serial_data)))
-
-            # 组消息包
-            process_dtu_data(serial_data)
-
-
         logger.debug("处理完成，休眠0.1秒")
-        time.sleep(0.1)
+        time.sleep(5)
 
