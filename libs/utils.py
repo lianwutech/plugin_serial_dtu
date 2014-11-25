a7b795073305adefb34c212f00d3bdd6674293e6
daviddeng@lianwutech.com <david.bb@2013>
2014-11-25 10:48:05 +0800

    优化插件架构

--- libs/utils.py
+++ libs/utils.py
@@ -3,6 +3,32 @@
 
 import os
 import sys
+import json
+
+
+def mkdir(path):
+    # 引入模块
+    import os
+
+    # 去除首位空格
+    path = path.strip()
+    # 去除尾部 \ 符号
+    path = path.rstrip("\\")
+
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists = os.path.exists(path)
+
+    # 判断结果
+    if not isExists:
+        # 如果不存在则创建目录
+        # 创建目录操作函数
+        os.makedirs(path)
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        return False
 
 def int2hex(int_value, sizeofint=2):
     """
@@ -56,3 +82,50 @@ def get_ip_addr(ifname="eth0"):
             return "127.0.0.1"
     else:
         return "127.0.0.1"
+
+
+def words_capitalize(words_str, split_char):
+    """
+    根据分割字符对字符串进行首字母大写并连接
+    :param str:
+    :param split_char:
+    :return:
+    """
+    result_str = ""
+    word_list = words_str.split(split_char)
+    for word in word_list:
+        result_str = result_str + word.capitalize()
+    return result_str
+
+
+def get_subclass(module, base_class):
+    """
+    获取子类的对象
+    :param module:
+    :param base_class:
+    :return:
+    """
+    for name in dir(module):
+        obj = getattr(module, name)
+        try:
+            if issubclass(obj, base_class) and obj != base_class:
+                return obj
+        except TypeError:  # If 'obj' is not a class
+            pass
+    return None
+
+
+def convert(input):
+    """
+    将字典或列表中unicode转成utf-8
+    :param input:
+    :return:
+    """
+    if isinstance(input, dict):
+        return {convert(key): convert(value) for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [convert(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
--- plugin.py
+++ plugin.py
@@ -8,21 +8,12 @@
     4、device_cmd内容：json字符串
 """
 
-import os
-import sys
-import time
-import json
 import serial
-import paho.mqtt.client as mqtt
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
-import threading
-import logging
-import ConfigParser
-from json import loads, dumps
 import mosquitto
 
-from libs.utils import *
 from libs.daemon import *
+from libs.plugin import *
 from libs.modbusdefine import *
 
 # 设置系统为utf-8  勿删除
@@ -30,79 +21,40 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 # 全局变量
+# 日志对象
+logger = logging.getLogger('plugin')
+
 # 设备信息字典
 devices_info_dict = dict()
+mqtt_client = None
+modbus_client = None
 
-# 切换工作目录
+# 配置信息
+config_info = load_config()
+
 # 程序运行路径
 procedure_path = cur_file_dir()
-# 工作目录修改为python脚本所在地址，后续成为守护进程后会被修改为'/'
+# 工作目录切换为python脚本所在地址，后续成为守护进程后会被修改为'/'
 os.chdir(procedure_path)
 # 通过工作目录获取当前插件名称
 plugin_name = procedure_path.split("/")[-1]
 
-# 日志对象
-logger = logging.getLogger('plugin')
-hdlr = logging.FileHandler('./plugin.log')
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-hdlr.setFormatter(formatter)
-logger.addHandler(hdlr)
-logger.setLevel(logging.DEBUG)
 
-# 加载配置项
-config = ConfigParser.ConfigParser()
-config.read("./plugin.cfg")
-serial_port = config.get('serial', 'port')
-serial_baund = int(config.get('serial', 'baund'))
-mqtt_server_ip = config.get('mqtt', 'server')
-mqtt_server_port = int(config.get('mqtt', 'port'))
-mqtt_client_id = config.get('mqtt', 'client_id')
-gateway_topic = config.get('gateway', 'topic')
-device_network = config.get('device', 'network')
-data_protocol = config.get('device', 'protocol')
-
-# 获取本机ip
-ip_addr = get_ip_addr()
-
-# 加载设备信息字典
-devices_info_file = "devices.txt"
-
-mqtt_client = None
-modbus_client = None
-
-
-def load_devices_info_dict():
-    if os.path.exists(devices_info_file):
-        devices_file = open(devices_info_file, "r+")
-        content = devices_file.read()
-        devices_file.close()
-        try:
-            devices_info_dict.update(json.loads(content))
-        except Exception, e:
-            logger.error("devices.txt内容格式不正确")
+# 检查系统配置项
+def check_config(config_info):
+    if "serial" in config_info and "mqtt" in config_info and "protocol" in config_info:
+        if "port" not in config_info["serial"] \
+                or "baund" not in config_info["serial"]:
+            return False
+        if "server" not in config_info["mqtt"] \
+                or "port" not in config_info["mqtt"] \
+                or "client_id" not in config_info["mqtt"] \
+                or "gateway_topic" not in config_info["mqtt"] \
+                or "network_name" not in config_info["mqtt"]:
+            return False
     else:
-        devices_file = open(devices_info_file, "w+")
-        devices_file.write("{}")
-        devices_file.close()
-    logger.debug("devices_info_dict加载结果%r" % devices_info_dict)
-
-
-# 新增设备
-def check_device(device_id, device_type, device_addr, device_port):
-    # 如果设备不存在则设备字典新增设备并写文件
-    if device_id not in devices_info_dict:
-        # 新增设备到字典中
-        devices_info_dict[device_id] = {
-            "device_id": device_id,
-            "device_type": device_type,
-            "device_addr": device_addr,
-            "device_port": device_port
-        }
-        logger.info("发现新设备%r" % devices_info_dict[device_id])
-        #写文件
-        devices_file = open(devices_info_file, "w+")
-        devices_file.write(dumps(devices_info_dict))
-        devices_file.close()
+        return False
+    return True
 
 
 def publish_device_data(device_id, device_type, device_addr, device_port, device_data):
@@ -113,13 +65,13 @@ def publish_device_data(device_id, device_type, device_addr, device_port, device
         "device_type": device_type,
         "device_addr": device_addr,
         "device_port": device_port,
-        "protocol": data_protocol,
+        "protocol": config_info["protocol"],
         "data": device_data
     }
 
     # MQTT发布
-    mqtt_client.publish(topic=gateway_topic, payload=json.dumps(device_msg))
-    logger.info("向Topic(%s)发布消息：%s" % (gateway_topic, device_msg))
+    mqtt_client.publish(topic=config_info["mqtt"]["gateway_topic"], payload=json.dumps(device_msg))
+    logger.info("向Topic(%s)发布消息：%s" % (config_info["mqtt"]["gateway_topic"], device_msg))
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -127,7 +79,7 @@ def on_connect(client, userdata, rc):
     logger.info("Connected with result code " + str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("%s/#" % device_network)
+    client.subscribe("%s/#" % config_info["mqtt"]["network_name"])
 
 
 # The callback for when a PUBLISH message is received from the server.
@@ -249,15 +201,15 @@ def on_message(client, userdata, msg):
 # 主函数
 class PluginDaemon(Daemon):
     def _run(self):
-
+        global devices_info_dict
         # 加载设备数据
-        load_devices_info_dict()
+        devices_info_dict = load_devices_info_dict()
 
         # 初始化modbus客户端
         global modbus_client
         modbus_client = ModbusClient(method='rtu',
-                                     port=serial_port,
-                                     baudrate=serial_baund,
+                                     port=config_info["serial"]["port"],
+                                     baudrate=config_info["serial"]["baund"],
                                      stopbits=serial.STOPBITS_ONE,
                                      parity=serial.PARITY_NONE,
                                      bytesize=serial.EIGHTBITS,
@@ -266,12 +218,12 @@ class PluginDaemon(Daemon):
 
         # 初始化mqtt客户端
         global mqtt_client
-        mqtt_client = mosquitto.Mosquitto(client_id=mqtt_client_id)
+        mqtt_client = mosquitto.Mosquitto(client_id=config_info["mqtt"]["client_id"])
         mqtt_client.on_connect = on_connect
         mqtt_client.on_message = on_message
 
         try:
-            mqtt_client.connect(mqtt_server_ip, mqtt_server_port, 60)
+            mqtt_client.connect(config_info["mqtt"]["server"], config_info["mqtt"]["port"], 60)
 
             # 发送设备信息
             for device_id in devices_info_dict:
